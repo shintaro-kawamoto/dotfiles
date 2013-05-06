2617c84f9a8fad95c9aa726710f358ed5c140632
KAWAMOTO Shintaro <haibaran@gmail.com>
2013-05-06 23:46:52 +0900

    fix create_symlink.sh

--- create_symlink.sh
+++ create_symlink.sh
@@ -2,3 +2,4 @@ ln -s dotfiles/.bashrc ~/.bashrc
 ln -s dotfiles/.bash_profile ~/.bash_profile
 ln -s dotfiles/.vimrc ~/.vimrc
 ln -s dotfiles/.gvimrc ~/.gvimrc
+ln -s dotfiles/.gitconfig ~/.gitconfig
