fcbf76567481f6e5aa71d6bdf8da9eb21f5bffcf
KAWAMOTO Shintaro <haibaran@gmail.com>
2014-08-02 14:50:34 +0900

    refactoring

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,32 +1,19 @@
-if [ -e ~/.bashrc ]; then
-    mv ~/.bashrc ~/.bashrc.bak
-    echo "move .bashrc .bashrc.bak"
-fi
-ln -s dotfiles/.bashrc ~/.bashrc
+#!/bin/sh
+cd $(dirname $0)
+for dotfile in .?*; do
+    case $dotfile in
+        ..)
+            continue;;
+        .git)
+            continue;;
+        *)
+            ln -is "${PWD}/${dotfile}" $HOME
+            ;;
+    esac
+done
 
-if [ -e ~/.bash_profile ]; then
-    mv ~/.bash_profile ~/.bash_profile.bak
-    echo "move .bash_profile .bash_profile.bak"
+if [ ! -d $HOME/vimbackup ]; then
+    mkdir $HOME/vimbackup
+    echo "create vimbackup directory."
 fi
-ln -s dotfiles/.bash_profile ~/.bash_profile
-
-if [ -e ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.bak
-    echo "move .vimrc .vimrc.bak"
-fi
-ln -s dotfiles/.vimrc ~/.vimrc
-mkdir ~/vimbackup
-
-if [ -e ~/.gvimrc ]; then
-    mv ~/.gvimrc ~/.gvimrc.bak
-    echo "move .gvimrc .gvimrc.bak"
-fi
-ln -s dotfiles/.gvimrc ~/.gvimrc
-
-if [ -e ~/.gitconfig ]; then
-    mv ~/.gitconfig ~/.gitconfig.bak
-    echo "move .gitconfig .gitconfig.bak"
-fi
-ln -s dotfiles/.gitconfig ~/.gitconfig
-
 
